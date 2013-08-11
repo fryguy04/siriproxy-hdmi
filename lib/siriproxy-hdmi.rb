6d7a7f061766a6db59d9fcf46897ae610413102e
Fred <fryguy04@gmail.com>
2013-08-11 03:17:07 -0400

    improved stable now

--- lib/siriproxy-hdmi.rb
+++ lib/siriproxy-hdmi.rb
@@ -2,54 +2,80 @@ require 'cora'
 require 'siri_objects'
 require 'pp'
 
-#######
-######
+# Examples: 
+#   Watch Tivo Everywhere
+#   Watch Apple in Basement
 
-class SiriProxy::Plugin::Hdmi < SiriProxy::Plugin
-  attr_accessor :deviceHash
+class SiriProxy::Plugin::Hdmi  < SiriProxy::Plugin
   attr_accessor :roomHash
+  attr_accessor :sourceHash
+  attr_accessor :ip
 
   def initialize(config)
-    @rooms   = Hash.new
-    @devices = Hash.new
-    @rooms   = config["roomHash"]
-    @devices = config["deviceHash"]
+    @aRooms   = Hash.new
+    @aSources = Hash.new
+    @aRooms   = config["roomHash"]
+    @aSources = config["sourceHash"]
+    @deviceIp = config["ip"]
 
   end
 
-  # Examples: 
-  #   Watch Tivo Everywhere
-  #   Watch Apple in Basement
+  
+  listen_for /(?:Watch|Launch) (.+)(?: ?TV)? (?:every where|everywhere)/i do |source|
 
-  # TODO: Passed in via config
+    # Sanity check, make sure Source is valid ... if not list valid sources and ask again
+    source = check_hash(source.downcase, @aSources)
 
-  listen_for /(?:Watch|Launch) (TiVo|DVD|Apple|PC)(?: ?TV)? (?:every where|everywhere)/i do |device|
-
-    say "Watching #{device} Everywhere" #say something to the user!
+	puts "Controlling: #{@deviceIp}"
+	
+    say "Watching #{source} Everywhere" #say something to the user!
 
     puts "**************************"
-    dev = deviceHash[device].to_s()
+    dev = @aSources[source].to_s()
 
     for room in 1..4
+	  puts "loop"
       cmd = room.to_s() + dev
       puts cmd
-      `echo #{cmd} | nc 192.168.0.70 4999`
+      `echo #{cmd} | nc #{@deviceIp} 4999`
       sleep 0.5
     end
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
   # 
-  listen_for /(?:Watch|Launch) (TiVo|DVD|Apple|PC)(?: ?TV)? (?:in|and)(?: the)? (family room|kitchen|bedroom|basement)/i do |device,room|
-    say "Watching #{device} in the #{room}" #say something to the user!
+  listen_for /(?:Watch|Launch) (\S+)(?: ?TV)? (?:in|and)(?: the)? (.*)/i do |source,room|
+
+    # Sanity check, make sure Source is valid ... if not list valid sources and ask again
+    source = check_hash(source.downcase, @aSources)
+    room   = check_hash(room.downcase, @aRooms)
+	
+	say "Watching #{source} in the #{room}" #say something to the user!
 
     puts "**************************"
-    cmd = roomHash[room].to_s() + deviceHash[device].to_s()
+    cmd = @aRooms[room].to_s() + @aSources[source].to_s()
     puts cmd
 
-    `echo #{cmd} | nc 192.168.0.70 4999`
+    `echo #{cmd} | nc #{@deviceIp} 4999`
 
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
+  
+  def check_hash(searchKey, generalHash)
+	puts "Validating: #{searchKey}"
+
+	if generalHash.has_key?(searchKey)
+		return searchKey	
+	else
+		say "Sorry, I could not find anything named #{searchKey}."
+		say "Here is the list of options."
+		generalHash.each_key {|searchKey| say searchKey.capitalize}
+		searchKey = ask "Which would you like to choose?"  
+		check_hash(searchKey.downcase.strip, generalHash)
+	end
+
+  end
+  
+  
 
 end
